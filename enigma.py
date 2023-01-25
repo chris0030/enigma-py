583b3fdd7ebb037ec70af9312f2001166cf613f7
Chris Weaver <chrisweaver@pop-os.localdomain>
2023-01-25 18:05:49 +0000

    Update Enigma to take rotator stack

--- enigma.py
+++ enigma.py
@@ -2,12 +2,15 @@ from enum import Enum
 import string
 from wiring import *
 
+ALPHABET = list(string.ascii_uppercase)
+
 def position_in_list(letter, letter_list):
+    # Returns index integer of letter in list
     return letter_list.index(letter)
 
 def alphabet_position(letter):
-    letter_list = list(string.ascii_uppercase)
-    return position_in_list(letter, letter_list)
+    # Returns index integer of letter in alphabet
+    return position_in_list(letter, ALPHABET)
 
 class RotorType(Enum):
     REGULAR = 1 
@@ -15,50 +18,38 @@ class RotorType(Enum):
     ENTRY_DISC = 3
 
 class Enigma:
-    def __init__(self, rotor_3, reflector, entry_disc=None, rotor_2=None, rotor_1=None, disable_rotate=False):
-        self.entry_disc = entry_disc
-        self.rotor_1 = rotor_1
-        self.rotor_2 = rotor_2
-        self.rotor_3 = rotor_3
-        self.reflector = reflector
-        self.disable_rotate = disable_rotate
+    def __init__(self, rotors):
+        self.rotors = rotors
 
     def display_rotor(self):
-        rotors = ""
-        rotors += f"{string.ascii_uppercase[self.reflector.position]}"
-        if self.rotor_1:
-            rotors += f"{string.ascii_uppercase[self.rotor_1.position]}"
-        if self.rotor_2:
-            rotors += f"{string.ascii_uppercase[self.rotor_2.position]}"
-        rotors += f"{string.ascii_uppercase[self.rotor_3.position]}"
-        return rotors
+        rotor_string = ""
+        for rotor in self.rotors:
+            rotor_string += f"{ALPHABET[rotor.position]}"
+        return rotor_string
 
     def reset(self):
         print("Resetting Enigma")
-        self.rotor_3.position = 0
-        if self.rotor_2:
-            self.rotor_2.position = 0
-        if self.rotor_1:
-            self.rotor_1.position = 0
+        for rotor in self.rotors:
+            rotor.position = 0
+
+    def process_rotation(self):
+        first = True
+        for rotor in reversed(self.rotors):
+            if first:
+                rotor.rotate()
+                first = False
+            if rotor.on_notch():
+                previous_rotor_index = self.rotors.index(rotor)
+                self.rotors[previous_rotor_index].rotate()
 
     def encode(self, letter):
-        self.rotor_3.rotate()
-        if self.rotor_3.on_notch():
-            self.rotor_2.rotate()
-            if self.rotor_2.on_notch():
-                self.rotor_1.rotate()
-        letter = self.rotor_3.encode_letter(letter)
-        if self.rotor_2:
-           letter = self.rotor_2.encode_letter(letter)
-        if self.rotor_1:
-            letter = self.rotor_1.encode_letter(letter)
-        letter = self.reflector.encode_letter(letter)
-        if self.rotor_1:
-            letter = self.rotor_1.decode_letter(letter)
-        if self.rotor_2:
-            letter = self.rotor_2.decode_letter(letter)
-        letter = self.rotor_3.decode_letter(letter)
+        self.process_rotation()
+        for rotor in reversed(self.rotors):
+            letter = rotor.encode_letter(letter)
+        for rotor in self.rotors[1:]:
+            letter = rotor.decode_letter(letter)
         return letter
+
 
 class Rotor:
     def __init__(self, rotor_type, letter_list, notch_list, ring_setting):
@@ -83,7 +74,7 @@ class Rotor:
         letter_number += self.position + self.ring_setting - 1
         letter_number = letter_number % 26
         new_letter = self.letter_list[letter_number]
-        return self.letter_list[letter_number]
+        return new_letter
 
     def decode_letter(self, letter):
         letter_number = position_in_list(letter, self.letter_list)
@@ -93,18 +84,31 @@ class Rotor:
         return new_letter
 
 if __name__ == "__main__":
-    rotor_1_letters = list(ROTOR_I['letters'])
-    rotor_1_notches = list(ROTOR_I['notch'])
-    reflector_letters = list(ROTOR_UKW_B['letters'])
-    rotor_2_letters = list(ROTOR_II['letters'])
-    rotor_2_notches = list(ROTOR_II['notch'])
-    rotor_3_letters = list(ROTOR_III['letters'])
-    rotor_3_notches = list(ROTOR_III['notch'])
-    reflector = Rotor(RotorType.REFLECTOR, reflector_letters, [], 1)
-    rotor_1 = Rotor(RotorType.REGULAR, rotor_1_letters, rotor_1_notches, 1)
-    rotor_2 = Rotor(RotorType.REGULAR, rotor_2_letters, rotor_2_notches, 1)
-    rotor_3 = Rotor(RotorType.REGULAR, rotor_3_letters, rotor_3_notches, 1)
-    enigma = Enigma(rotor_1, reflector, rotor_2, rotor_3)
+    reflector = Rotor(
+        RotorType.REFLECTOR,
+        ROTORS['ROTOR_UKW_C']['letters'],
+        [],
+        1
+    )
+    rotor_1 = Rotor(
+        RotorType.REGULAR,
+        ROTORS['ROTOR_I']['letters'],
+        ROTORS['ROTOR_I']['notch'],
+        1
+    )
+    rotor_2 = Rotor(
+        RotorType.REGULAR,
+        ROTORS['ROTOR_II']['letters'],
+        ROTORS['ROTOR_II']['notch'],
+        1
+    )
+    rotor_3 = Rotor(
+        RotorType.REGULAR,
+        ROTORS['ROTOR_III']['letters'],
+        ROTORS['ROTOR_III']['notch'],
+        1
+    )
+    enigma = Enigma([reflector, rotor_1, rotor_2, rotor_3])
     result = ""
     for letter in list("CHRIS".replace(" ", "X").upper()):
         print(enigma.display_rotor())
@@ -112,7 +116,7 @@ if __name__ == "__main__":
     print(result)
     enigma.reset()
     result = ""
-    for letter in list("RGSHZ"):
+    for letter in list("FZCWD"):
         print(enigma.display_rotor())
         result += enigma.encode(letter)
     print(result)
